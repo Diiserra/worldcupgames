d9f27a9ccc4fdfd231ea751367b6cfaba5edb89f
Diego Lima <diiserras@gmail.com>
2022-05-27 21:11:34 -0300

    DS-05 HomePage Create

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,15 @@
+import styles from "../styles/HomePage.module.css";
 
 export default function HomePage() {
-    return (
-        <div>Home Page</div>
-    )
+  return (
+    <div className={styles.backGround}>
+      <div className={styles.container}>
+        <img src="./src/assets/worldcuplogo.svg" alt="qatar logo" />
+        <div className={styles.titleContainer}>
+          <h1>Vamos torcer juntos!</h1>
+          <p>A Copa do Mundo FIFA Qatar 2022™, está chegando cheia de emoção para nos fazer torcer e vibrar pelo o nosso país. Estamos contando as horas para ver esse espataculo.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
