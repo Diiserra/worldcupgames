53a3248ddadda85641d5139ecbca9761993139b7
Diego Lima <diiserras@gmail.com>
2022-05-30 17:09:05 -0300

    DS-07 CountdownFunction

--- frontend/src/components/CountdownComponent.tsx
+++ frontend/src/components/CountdownComponent.tsx
@@ -1,26 +1,53 @@
-import styles from "../styles/CountdownComponent.module.css"
-
+import { useEffect, useState } from "react";
+import styles from "../styles/CountdownComponent.module.css";
 
 export default function CountdownComponent() {
-    return (
-        <div className={styles.cardContainer}>
-            <span className={styles.titleCountdown}>Contagem regressiva para o Qatar 2022™</span>
-            <div className={styles.countdownContainer}>
-                <div className={styles.countdown}>
-                    <span>177</span>
-                    <span>DIAS</span>
-                </div>
-                <strong>◆</strong>
-                <div className={styles.countdown}>
-                    <span>08</span>
-                    <span>HORAS</span>
-                </div>
-                <strong>◆</strong>
-                <div className={styles.countdown}>
-                    <span>43</span>
-                    <span>MINUTOS</span>
-                </div>
-            </div>
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+
+  const countdownDate = () => {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const dateNow = new Date(Date.now()).getTime();
+    const dateQatarWorldCup = new Date(2022, 10, 21, 7).getTime();
+    const dateDifference = dateQatarWorldCup - dateNow;
+
+    setDays(Math.floor(dateDifference / day));
+    setHours(Math.floor((dateDifference % day) / hour));
+    setMinutes(Math.floor((dateDifference % hour) / minute));
+  };
+
+  useEffect(() => {
+    setInterval(() => {
+      countdownDate();
+    }, 1000);
+  }, []);
+
+  return (
+    <div className={styles.cardContainer}>
+      <span className={styles.titleCountdown}>
+        Contagem regressiva para o Qatar 2022™
+      </span>
+      <div className={styles.countdownContainer}>
+        <div className={styles.countdown}>
+          <span>{days}</span>
+          <span>DIAS</span>
         </div>
-    )
+        <strong>◆</strong>
+        <div className={styles.countdown}>
+          <span>{hours}</span>
+          <span>HORAS</span>
+        </div>
+        <strong>◆</strong>
+        <div className={styles.countdown}>
+          <span>{minutes}</span>
+          <span>MINUTOS</span>
+        </div>
+      </div>
+    </div>
+  );
 }
