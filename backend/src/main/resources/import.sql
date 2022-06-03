fc45603fa48185e0f4afde1b6735e03a19ea3ce1
Diego Lima <diiserras@gmail.com>
2022-06-03 16:16:25 -0300

    DS-12 Team and group seed database

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,48 @@
+INSERT INTO tb_group (description, group_name) VALUES('Grupo de classificação A', 'A');
+INSERT INTO tb_group (description, group_name) VALUES('Grupo de classificação B', 'B');
+INSERT INTO tb_group (description, group_name) VALUES('Grupo de classificação C', 'C');
+INSERT INTO tb_group (description, group_name) VALUES('Grupo de classificação D', 'D');
+INSERT INTO tb_group (description, group_name) VALUES('Grupo de classificação E', 'E');
+INSERT INTO tb_group (description, group_name) VALUES('Grupo de classificação F', 'F');
+INSERT INTO tb_group (description, group_name) VALUES('Grupo de classificação G', 'G');
+INSERT INTO tb_group (description, group_name) VALUES('Grupo de classificação H', 'H');
+
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Catar', 'ct', 'Seleção do Catar', 1);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Equador', 'eq', 'Seleção do Equador', 1);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Senegal', 'sg', 'Seleção do Senegal', 1);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Holanda', 'hl', 'Seleção do Holanda', 1);
+
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Inglaterra', 'ig', 'Seleção do Inglaterra', 2);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Ir�', 'ir', 'Seleção do Ir�', 2);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Estados Unidos', 'us', 'Seleção do Estados Unidos', 2);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('ICP 3', 'aa', 'Seleção do AA', 2);
+
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Argentina', 'ar', 'Seleção do Argentina', 3);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Ar�bia Saudita', 'as', 'Seleção do Ar�bia Saudita', 3);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('M�xico', 'mx', 'Seleção do M�xico', 3);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Pol�nia', 'aa', 'Seleção do Pol�nia', 3);
+
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Fran�a', 'fr', 'Seleção do Fran�a', 4);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('ICP1', 'aa', 'Seleção do ICP1', 4);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Dinamarca', 'aa', 'Seleção do Dinamarca', 4);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Tunisia', 'aa', 'Seleção do Tunisia', 4);
+
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Espanha', 'es', 'Seleção do Espanha', 5);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('ICP2', 'aa', 'Seleção do ICP2', 5);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Alemanha', 'al', 'Seleção do Alemanha', 5);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Jap�o', 'jp', 'Seleção do Jap�o', 5);
+
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('B�lgica', 'bg', 'Seleção do B�lgica', 6);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Canad�', 'cd', 'Seleção do Canad�', 6);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Marrocos', 'mr', 'Seleção do Marrocos', 6);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Cro�cia', 'cr', 'Seleção do Cro�cia', 6);
+
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Brasil', 'br', 'Seleção do Brasil', 7);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('S�rvia', 'sv', 'Seleção do S�rvia', 7);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Sui�a', 'su', 'Seleção do Sui�a', 7);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Camar�es', 'cm', 'Seleção do Camar�es', 7);
+
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Portugal', 'cm', 'Seleção do Portugal', 8);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Gana', 'cm', 'Seleção do Gana', 8);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Uruguai', 'cm', 'Seleção do Uruguai', 8);
+INSERT INTO tb_team (country, initials, team_name, group_id) VALUES('Coreia do Sul', 'cm', 'Seleção do Coreia do Sul', 8);
