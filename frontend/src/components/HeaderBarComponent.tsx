665a8dd5701af8b40f64a7913cbc876c47b22073
Diego Lima <diiserras@gmail.com>
2022-06-09 22:38:43 -0300

    DS-14 HeaderBar dinamic style

--- frontend/src/components/HeaderBarComponent.tsx
+++ frontend/src/components/HeaderBarComponent.tsx
@@ -1,11 +1,38 @@
+import React, { LinkHTMLAttributes, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../styles/HeaderBarComponent.module.css";
 
 export default function HeaderBarComponent() {
 
+  const [homePageActive, setHomePageActive] = useState(true);
+
+  const [menuActive, setMenuActive] = useState('Início');
+
+  const headerHandler = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    const a = event.currentTarget.innerHTML;
+
+    switch (a) {
+      case 'Grupos':
+        setHomePageActive(false)
+        setMenuActive(a)
+        break;
+      case 'Tabela':
+        setHomePageActive(false)
+        setMenuActive(a)
+        break;
+      case 'Notícias':
+        setHomePageActive(false)
+        setMenuActive(a)
+        break;
+      default:
+        setHomePageActive(true)
+        setMenuActive(a)
+        break;
+    }
+  }
 
   return (
-    <header className={styles.container}>
+    <header className={homePageActive ? styles.homePageHeader : styles.normalHeader}>
       <a href="https://www.qatar2022.qa/en/home">
         <img
           className={styles.logoWorldCup}
@@ -16,16 +43,16 @@ export default function HeaderBarComponent() {
       <nav className={styles.menuNavContainer}>
         <ul>
           <li>
-            <Link className={styles.menuActive} to="/">Início</Link>
+            <Link className={menuActive === 'Início' ? styles.menuActive : styles.menuDesable} to="/" onClick={event =>headerHandler(event)}>Início</Link>
           </li>
           <li>
-            <Link className={styles.menuDesable} to="/grupos">Grupos</Link>
+            <Link className={menuActive === 'Grupos' ? styles.menuActive : styles.menuDesable} to="/grupos" onClick={event =>headerHandler(event)}>Grupos</Link>
           </li>
           <li>
-            <Link className={styles.menuDesable} to="/tabela">Tabela</Link>
+            <Link className={menuActive === 'Tabela' ? styles.menuActive : styles.menuDesable} to="/tabela" onClick={event =>headerHandler(event)}>Tabela</Link>
           </li>
           <li>
-            <Link className={styles.menuDesable} to="noticias">Notícias</Link>
+            <Link className={menuActive === 'Notícias' ? styles.menuActive : styles.menuDesable} to="noticias" onClick={event =>headerHandler(event)}>Notícias</Link>
           </li>
         </ul>
       </nav>
